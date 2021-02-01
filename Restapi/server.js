61c4d65040151b2d6c14742a60b870686316a162
msumari <suleykhery@gmail.com>
2021-02-01 14:40:30 +0300

    changes

--- Restapi/server.js
+++ Restapi/server.js
@@ -1,25 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-// const { JsonDB } = require('node-json-db')
-// const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// const db = new JsonDB(new Config('apitest', true, false, '/'));
+const PORT = process.env.PORT || 3000;
 
-// const mongoose = require('mongoose')
-
-// mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true},{ useUnifiedTopology: true })
-// const db = mongoose.connection
-
-// db.on('error', (error)=> console.error(error))
-// db.once('open', () => console.log('connected to database'))
-
-const testRoute = require('./routes/test')
-app.use('/test', testRoute)
-app.use(express.json())
-
- 
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => console.log(`api live on port ${PORT}`))
+app.listen(PORT, () => console.log(`api live on port ${PORT}`));
