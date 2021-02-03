8550ef10caa149467e7af8733b68f40300441228
msumari <suleykhery@gmail.com>
2021-02-03 14:34:35 +0300

    port

--- Restapi/server.js
+++ Restapi/server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log("LOGGED");
+  next();
+};
+
+const requestTime = (req, res, next) => {
+  req.reqTime = Date.now();
+  next();
+};
+
+app.use(logger);
+app.use(requestTime);
+
+app.get("/time", (req, res) => {
+  res.send(`Current time:${req.reqTime}`);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World! My name is Msumari");
+  res.send("Home page");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("Node api");
 });
 
 const PORT = process.env.PORT || 3000;
